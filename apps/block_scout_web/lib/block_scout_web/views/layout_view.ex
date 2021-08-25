81800110b1b10beb1d1aa3fde8414519986ff7ff
libbin <30615691+libbin@users.noreply.github.com>
2021-08-25 23:10:16 +0800

    Update layout_view.ex
    update repo and base url

--- apps/block_scout_web/lib/block_scout_web/views/layout_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/layout_view.ex
@@ -4,15 +4,15 @@ defmodule BlockScoutWeb.LayoutView do
   alias Plug.Conn
   alias Poison.Parser
 
-  @issue_url "https://github.com/elastos/Elastos.ELA.SideChain.ETH.Blockscout/issues/new/choose"
+  @issue_url "https://github.com/elastos/Elastos.ELA.SideChain.ESC.Blockscout/issues/new/choose"
   @default_other_networks [
     %{
       title: "Mainnet",
-      url: "https://eth.elastos.io",
+      url: "https://esc.elastos.io",
     },
     %{
       title: "Testnet",
-      url: "https://eth-testnet.elastos.io",
+      url: "https://esc-testnet.elastos.io",
       test_net?: true
     },
   ]
