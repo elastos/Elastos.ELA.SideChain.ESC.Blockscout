26f99abf8542bab1144318d42d144906cbba7bf0
libbin <30615691+libbin@users.noreply.github.com>
2021-08-26 17:42:54 +0800

    Update layout_view.ex
    update repo url

--- apps/block_scout_web/lib/block_scout_web/views/layout_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/layout_view.ex
@@ -103,7 +103,7 @@ defmodule BlockScoutWeb.LayoutView do
           nil
 
         release_link_env_var == "" || release_link_env_var == nil ->
-          "https://github.com/elastos/Elastos.ELA.SideChain.ETH.Blockscout/tree/" <> version
+          "https://github.com/elastos/Elastos.ELA.SideChain.ESC.Blockscout/tree/" <> version
 
         true ->
           release_link_env_var
