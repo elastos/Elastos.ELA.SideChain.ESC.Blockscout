2b4c1c59d8b35b3ac88a20d5c11f15b32b665db9
free1 <freeloverails@gmail.com>
2022-06-23 15:58:15 +0800

    add ETHEREUM_JSONRPC_DEBUG_TRACE_TRANSACTION_TIMEOUT param to trace transaction

--- apps/ethereum_jsonrpc/config/config.exs
+++ apps/ethereum_jsonrpc/config/config.exs
@@ -26,7 +26,8 @@ config :ethereum_jsonrpc, EthereumJSONRPC.Tracer,
   adapter: SpandexDatadog.Adapter,
   trace_key: :blockscout
 
-config :ethereum_jsonrpc, EthereumJSONRPC.Geth, debug_trace_transaction_timeout: "60s"
+debug_trace_transaction_timeout = System.get_env("ETHEREUM_JSONRPC_DEBUG_TRACE_TRANSACTION_TIMEOUT", "5s")
+config :ethereum_jsonrpc, EthereumJSONRPC.Geth, debug_trace_transaction_timeout: debug_trace_transaction_timeout
 
 config :logger, :ethereum_jsonrpc,
   # keep synced with `config/config.exs`
