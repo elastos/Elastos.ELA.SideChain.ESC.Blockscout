ea14e82cdb1a958c1a832b196b7620f01524055e
free1 <freeloverails@gmail.com>
2023-06-21 16:07:54 +0800

    change receipts setting test

--- apps/indexer/lib/indexer/block/fetcher.ex
+++ apps/indexer/lib/indexer/block/fetcher.ex
@@ -71,8 +71,8 @@ defmodule Indexer.Block.Fetcher do
   # These are all the *default* values for options.
   # DO NOT use them directly in the code.  Get options from `state`.
 
-  @receipts_batch_size 6
-  @receipts_concurrency 6
+  @receipts_batch_size 10
+  @receipts_concurrency 10
 
   @doc false
   def default_receipts_batch_size, do: @receipts_batch_size
